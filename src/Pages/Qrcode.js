b891439e74202a3309eb0e0552a70866d0d703f4
Mxrrega <matheusmarregamarrega@gmail.com>
2024-11-06 15:33:21 -0300

    Alterações no Qr code

--- src/Pages/Qrcode.js
+++ src/Pages/Qrcode.js
@@ -1,43 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator, TouchableOpacity } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import { View, Text, StyleSheet, ActivityIndicator, TouchableOpacity, Image } from 'react-native';
+import { Camera  } from 'expo-camera';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function QRCodeScanner({ handle }) {
+
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [machineDetails, setMachineDetails] = useState(null);
+  const [maquina, setMaquina] = useState(null);
+  const [tipoMaquinaNome, setTipoMaquinaNome ] = useState(null);
+  const [tipoMaquina, setTipoMaquina] = useState([]);
 
-  useEffect(() => {
+  /*useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      const { status } = await Camera.requestPermissionsAsync();
       setHasPermission(status === 'granted');
     };
 
     getBarCodeScannerPermissions();
-  }, []);
+  }, []);*/
 
   const handleBarCodeScanned = async ({ data }) => {
     setScanned(true);
     setLoading(true);
 
-    await fetch(`${process.env.EXPO_PUBLIC_URL}/api/Maquina/GetMaquinaById/${data}`, {
-        method: 'GET',
-        headers: {
-          'content-Type': 'application/json',
-        },
-      })
+    await fetch(`${process.env.EXPO_PUBLIC_URL}/api/Maquina/GetMaquinaByNumeroSerie/${data}`, {
+      method: 'GET',
+      headers: {
+        'content-Type': 'application/json',
+      },
+    })
       .then(res => res.json())
-      .then(json => setMachineDetails(json))
-      .catch(err => console.error('Erro ao carregar tipos de máquinas:', err));
+      .then(json => setMaquina(json))
+      .catch(err => console.error('Erro ao carregar máquina:', err))
+      .finally(() => setLoading(false));
+      
 
+      
 
   };
 
-  if (hasPermission === null) {
-    return <Text>Solicitando permissão para acessar a câmera...</Text>;
+  async function getTipoMaquina()
+  {
+    await fetch(`${process.env.EXPO_PUBLIC_URL}/api/TipoMaquina/GetTipoMaquinaById/${maquina.tipoMaquinaId}`, {
+      method: 'GET',
+      headers: {
+        'content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(json => setTipoMaquinaNome( json.tipoMaquinaNome ) )
+      .catch(err => console.error('Erro ao carregar Tipo de Máquina:', err))
   }
+
+  useEffect( () => {
+    if( maquina ) {
+      getTipoMaquina();
+    }
+  }, [maquina])
+
   if (hasPermission === false) {
     return <Text>Permissão de câmera negada</Text>;
   }
@@ -50,36 +72,73 @@ export default function QRCodeScanner({ handle }) {
         </TouchableOpacity>
         <Text style={styles.headerTitle}>Escanear QR Code</Text>
       </View>
-      {!machineDetails ? (
-        <View>
-          <BarCodeScanner
+      {!maquina ? (
+        <View style={styles.cameraBox}>
+          <View style={styles.cameraBorder}>
+          <Camera
             onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
             style={styles.camera}
           />
+          </View>
           {scanned && loading ? (
             <ActivityIndicator size="large" color="#888" style={styles.loadingIndicator} />
           ) : (
-            <Text style={styles.instructionText}>Carregando informações</Text>
+            <Text style={styles.instructionText}>Escaneie o QR Code</Text>
           )}
         </View>
       ) : (
         <View style={styles.detailsContainer}>
-          <Text style={styles.detailsText}>Detalhes da Máquina</Text>
-          <Text style={styles.detailsText}>Nome: {machineDetails.nome}</Text>
-          <Text style={styles.detailsText}>Tipo de Máquina ID: {machineDetails.tipoMaquinaId}</Text>
-          <Text style={styles.detailsText}>Setor ID: {machineDetails.setorId}</Text>
-          <Text style={styles.detailsText}>Modelo: {machineDetails.modelo}</Text>
-          <Text style={styles.detailsText}>Número de Série: {machineDetails.numeroSerie}</Text>
-          <Text style={styles.detailsText}>Fabricante ID: {machineDetails.fabricanteId}</Text>
-          <Text style={styles.detailsText}>Data de Aquisição: {new Date(machineDetails.dataAquisicao).toLocaleDateString()}</Text>
-          <Text style={styles.detailsText}>Peso: {machineDetails.peso} kg</Text>
-          <Text style={styles.detailsText}>Voltagem: {machineDetails.voltagem}V</Text>
-          <Text style={styles.detailsText}>Detalhes: {machineDetails.maquinaDetalhes}</Text>
-          <Text style={styles.scanAgain} onPress={() => {
-            setMachineDetails(null);
-            setScanned(false);
-          }}>Escanear novamente</Text>
+        <Text style={styles.detailsTitle}>Detalhes da Máquina</Text>
+        <Image style={styles.image} source={{ uri: maquina.fotoUrl }} />
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Nome:</Text>
+          <Text style={styles.value}>{maquina.nome}</Text>
         </View>
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Tipo de Máquina ID:</Text>
+          <Text style={styles.value}>{tipoMaquinaNome}</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Setor ID:</Text>
+          <Text style={styles.value}>{maquina.setorId}</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Modelo:</Text>
+          <Text style={styles.value}>{maquina.modelo}</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Número de Série:</Text>
+          <Text style={styles.value}>{maquina.numeroSerie}</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Fabricante ID:</Text>
+          <Text style={styles.value}>{maquina.fabricanteId}</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Data de Aquisição:</Text>
+          <Text style={styles.value}>{new Date(maquina.dataAquisicao).toLocaleDateString()}</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Peso:</Text>
+          <Text style={styles.value}>{maquina.peso} kg</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Voltagem:</Text>
+          <Text style={styles.value}>{maquina.voltagem}V</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Detalhes:</Text>
+          <Text style={styles.value}>{maquina.maquinaDetalhes}</Text>
+        </View>
+        <TouchableOpacity style={styles.button}
+        onPress={() => {
+          setMaquina(null);
+          setScanned(false);
+        }}>
+        <Text style={styles.scanAgain} >Escanear novamente</Text>
+        </TouchableOpacity>
+        
+      </View>
       )}
     </View>
   );
@@ -94,24 +153,74 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 20,
+    justifyContent: 'center',
+    marginTop: 40,
     marginBottom: 20,
-    textAlign: 'center',
-    alignContent: 'center'
   },
   iconButton: {
+    position: 'absolute',
+    left: 0,
     padding: 8,
   },
   headerTitle: {
     color: '#FFFFFF',
     fontSize: 20,
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+  },
+  cameraBox: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cameraBorder: {
+    width: 350,
+    height: 350,
+    borderWidth: 2,
+    borderColor: '#1e90ff',
+    borderRadius: 12,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   camera: {
-    width: '80%',
-    height: '50%',
-    marginVertical: 20,
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#1C1C1E',
+    padding: 16,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    marginBottom: 20,
+  },
+  iconButton: {
+    position: 'absolute',
+    left: 0,
+    padding: 8,
+  },
+  headerTitle: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  cameraBox: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  camera: {
+    width: 350,
+    height: 350,
+    borderWidth: 2,
+    borderColor: '#1e90ff',
+    borderRadius: 12,
   },
   loadingIndicator: {
     marginTop: 20,
@@ -119,23 +228,54 @@ const styles = StyleSheet.create({
   instructionText: {
     color: '#888',
     fontSize: 16,
-    fontStyle: 'italic',
     marginTop: 20,
+    fontWeight: 'bold',
   },
   detailsContainer: {
-    alignItems: 'center',
     padding: 20,
   },
-  detailsText: {
-    fontSize: 18,
+  detailsTitle: {
+    fontSize: 22,
     fontWeight: 'bold',
-    color: '#fff',
-    marginBottom: 10,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  image: {
+    width: '100%',
+    height: 150,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  infoBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#444',
+  },
+  label: {
+    fontSize: 16,
+    color: '#888',
+  },
+  value: {
+    fontSize: 16,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
   },
   scanAgain: {
-    color: '#1e90ff',
+    color: '#FFF',
     fontSize: 16,
-    marginTop: 20,
-    textDecorationLine: 'underline',
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#696767',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+    height: 70
   },
 });
